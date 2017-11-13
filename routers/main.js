cbc7319397c41ec31840fcd40a93e9e3b6b6cc2f
xiaopanke <1547947237@qq.com>
2017-11-13 16:19:46 +0800

    详情页

--- routers/main.js
+++ routers/main.js
@@ -2,26 +2,38 @@ var express=require('express');
 var router=express.Router();
 var Category=require('../models/Category')
 var Content=require('../models/Content')
+
+var data;
+//处理能用的数据
+router.use((req,res,next) => {
+  data={
+    userInfo:req.userInfo,
+    categories:[],
+  }
+    Category.find().then((categories) => {
+      data.categories=categories;
+      next();
+    });
+})
+
 //首页
 router.get('/',(req,res,next) => {
-  var data={
-    userInfo:req.userInfo,
-    category:req.query.category || '',
-    categories:[],
-    page:Number(req.query.page) || 1,
-    limit:3,
-    pages:0,
-    count:0
+  data={
+    ...data,
+    ...{
+      category:req.query.category || '',
+      page:Number(req.query.page) || 1,
+      limit:3,
+      pages:0,
+      count:0
+    }
   }
   var where={};
   if(data.category){
     where.category=data.category
   }
   //从数据库中读取分类信息
-  Category.find().then((categories) => {
-    data.categories=categories;
-    return Content.where(where).count();
-  }).then((count) => {
+  Content.where(where).count().then((count) => {
     data.count=count;
     data.pages=Math.ceil(count/data.limit);
     //取值不能超过pages
@@ -33,10 +45,22 @@ router.get('/',(req,res,next) => {
     return Content.where(where).find().limit(data.limit).skip(skip).populate(['category','user']).sort({addTime:-1})
   }).then((contents) => {
     data.contents=contents;
-    console.log(data);
     res.render('main/index',data)
   })
 
+})
+
+//阅读全文
+router.get('/view',(req,res) => {
+  let {contentid}=req.query;
+  Content.findOne({
+    _id:contentid
+  }).then((content) => {
+    data.content=content;
+    content.views++;
+    content.save();
+    res.render('main/view',data)
+  })
 })
 
 module.exports=router;
