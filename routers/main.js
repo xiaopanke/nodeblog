d7aa829f74d75a2441af1e61c315182e72f30fba
xiaopanke <1547947237@qq.com>
2017-11-13 15:35:27 +0800

    首页分页，导航跳转，导航的高亮

--- routers/main.js
+++ routers/main.js
@@ -6,16 +6,21 @@ var Content=require('../models/Content')
 router.get('/',(req,res,next) => {
   var data={
     userInfo:req.userInfo,
+    category:req.query.category || '',
     categories:[],
     page:Number(req.query.page) || 1,
     limit:3,
     pages:0,
     count:0
   }
+  var where={};
+  if(data.category){
+    where.category=data.category
+  }
   //从数据库中读取分类信息
   Category.find().then((categories) => {
     data.categories=categories;
-    return Content.count();
+    return Content.where(where).count();
   }).then((count) => {
     data.count=count;
     data.pages=Math.ceil(count/data.limit);
@@ -24,7 +29,8 @@ router.get('/',(req,res,next) => {
     //取值不能小于1
     data.page=Math.max(data.page,1)
     let skip=(data.page-1)*data.limit;
-    return Content.find().limit(data.limit).skip(skip).populate(['category','user']).sort({addTime:-1})
+
+    return Content.where(where).find().limit(data.limit).skip(skip).populate(['category','user']).sort({addTime:-1})
   }).then((contents) => {
     data.contents=contents;
     console.log(data);
